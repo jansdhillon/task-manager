0e01aa83ae91c0bd9975dd46965b804025613a82
jansdhillon <imightbejan@gmail.com>
2025-02-10 10:53:33 -0700

    add supabse secrets

--- cmd/task-manager/main.go
+++ cmd/task-manager/main.go
@@ -3,23 +3,49 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+type SupabaseClient struct {
+	SupabaseUrl            string
+	SupabaseAnonKey        string
+	SupabaseServiceRoleKey string
+}
+
+func rootHandler(client *SupabaseClient) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+
+		fmt.Fprintln(w, "Task Manager")
 	}
 
-	fmt.Fprintln(w, "Task Manager")
 }
 
 func main() {
-	http.HandleFunc("/", rootHandler)
+
+	client := &SupabaseClient{
+		SupabaseUrl:            getEnv("SUPABASE_URL"),
+		SupabaseAnonKey:        getEnv("SUPABASE_ANON_KEY"),
+		SupabaseServiceRoleKey: getEnv("SUPABASE_SERVICE_ROLE_KEY"),
+	}
+
+	http.HandleFunc("/", rootHandler(client))
 
 	fmt.Printf("Server starting on port 8080\n")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
+}
+
+func getEnv(key string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		fmt.Printf("Env var %s not found, exiting...", value)
+		os.Exit(1)
+	}
+	return value
 }
