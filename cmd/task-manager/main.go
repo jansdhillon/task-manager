872d521719c1ad92f32d10cdea4a8861d1dd32a7
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2025-05-24 10:43:11 -0600

    add cli (rough)

--- cmd/task-manager/main.go
+++ cmd/task-manager/main.go
@@ -5,13 +5,18 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	task "github.com/jansdhillon/task-manager/internal/task"
 	"github.com/urfave/cli/v3"
 )
 
-func main() {
+const (
+	listFlag = "list"
+)
+
+func newApp() *cli.Command {
 	tasks := make([]task.Task, 0, task.MAX_TASKS)
 
 	store := &task.InMemoryTaskStore{
@@ -22,10 +27,42 @@ func main() {
 	for i := range task.MAX_TASKS {
 		description := "world"
 		task := task.New(fmt.Sprintf("Task #%d", i), &description)
-		addedTask := store.AddTask(task)
-		fmt.Printf("Added task: %s\n", addedTask.String())
+		store.AddTask(task)
 	}
 
-	(&cli.Command{}).Run(context.Background(), os.Args)
+	listCmd := &cli.Command{
+		Name:  "tasks",
+		Usage: "Show tasks in the store.",
+		Action: func(context.Context, *cli.Command) error {
+			for _, task := range store.Tasks {
+				fmt.Printf("Task: %s\n", task.String())
+			}
+			return nil
+		},
+	}
+	return &cli.Command{
+		Usage: "Manage tasks.",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    listFlag,
+				Aliases: []string{"l"},
+				Usage:   "List the tasks.",
+			},
+		},
+		Commands: []*cli.Command{
+			listCmd,
+		},
+	}
+}
 
+func actionSetup(c *cli.Command) (err error) {
+	return nil
+}
+
+func main() {
+	app := newApp()
+
+	if err := app.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
