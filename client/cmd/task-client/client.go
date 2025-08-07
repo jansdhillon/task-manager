b1528a5c843d5615b42138cf90677e8e68f4c6a5
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2025-08-06 23:10:46 -0600

    init client, fix df

---
+++ client/cmd/task-client/client.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	pb "github.com/jansdhillon/task-manager/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type TaskClient struct {
+	client pb.TaskServiceClient
+	conn   *grpc.ClientConn
+}
+
+func NewTaskClient(address string) (*TaskClient, error) {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to gRPC server: %w", err)
+	}
+
+	client := pb.NewTaskServiceClient(conn)
+	return &TaskClient{
+		client: client,
+		conn:   conn,
+	}, nil
+}
+
+func (c *TaskClient) Close() error {
+	return c.conn.Close()
+}
+
+func (c *TaskClient) CreateTask(ctx context.Context, title string, description *string) (*pb.Task, error) {
+	req := &pb.CreateTaskRequest{
+		Title: title,
+	}
+	if description != nil {
+		req.Description = description
+	}
+
+	resp, err := c.client.CreateTask(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create task: %w", err)
+	}
+
+	return resp.Task, nil
+}
+
+func (c *TaskClient) GetTask(ctx context.Context, id string) (*pb.Task, error) {
+	req := &pb.GetTaskRequest{
+		Id: id,
+	}
+
+	resp, err := c.client.GetTask(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get task: %w", err)
+	}
+
+	return resp.Task, nil
+}
+
+func (c *TaskClient) UpdateTask(ctx context.Context, id, title string, description *string) (*pb.Task, error) {
+	req := &pb.UpdateTaskRequest{
+		Id:    id,
+		Title: title,
+	}
+	if description != nil {
+		req.Description = description
+	}
+
+	resp, err := c.client.UpdateTask(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update task: %w", err)
+	}
+
+	return resp.Task, nil
+}
+
+func (c *TaskClient) DeleteTask(ctx context.Context, id string) (bool, error) {
+	req := &pb.DeleteTaskRequest{
+		Id: id,
+	}
+
+	resp, err := c.client.DeleteTask(ctx, req)
+	if err != nil {
+		return false, fmt.Errorf("failed to delete task: %w", err)
+	}
+
+	return resp.Success, nil
+}
+
+func executeWithClient(address string, fn func(*TaskClient) (any, error)) (any, error) {
+	c, err := NewTaskClient(address)
+	if err != nil {
+		log.Printf("error creating client: %v", err)
+		return nil, err
+	}
+
+	res, err := fn(c)
+
+	return res, err
+}
--- server/go.mod
+++ server/go.mod
@@ -4,16 +4,14 @@ go 1.24.2
 
 require (
 	github.com/google/uuid v1.6.0
-	github.com/jansdhillon/task-manager/proto v0.0.0-00010101000000-000000000000
+	github.com/jansdhillon/task-manager/proto v0.0.0-20250807034741-e522fe866932
 	google.golang.org/grpc v1.73.0
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
-	golang.org/x/net v0.38.0 // indirect
-	golang.org/x/sys v0.31.0 // indirect
-	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
 )
-
-replace github.com/jansdhillon/task-manager/proto => ../proto
