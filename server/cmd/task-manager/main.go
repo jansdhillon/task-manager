2ee9232a93e152baf88ffaa2f3e9802867d1b580
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2025-08-10 20:56:10 -0600

    init pgx

--- server/cmd/task-manager/main.go
+++ server/cmd/task-manager/main.go
@@ -3,14 +3,38 @@ package main
 import (
 	"log"
 	"net"
+	"time"
+
+	"context"
+	"fmt"
+	"os"
 
 	"github.com/jansdhillon/task-manager/server/internal/server"
 	"github.com/jansdhillon/task-manager/server/internal/task"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var id int
+	var created_at time.Time
+	err = conn.QueryRow(context.Background(), "select id, created_at from task").Scan(&id, &created_at)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(id, created_at)
+
 	store := &task.InMemoryTaskStore{
 		Name:  "TaskManager",
 		Tasks: make([]task.Task, 0, task.MAX_TASKS),
--- server/go.mod
+++ server/go.mod
@@ -10,6 +10,12 @@ require (
 )
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jackc/pgx/v5 v5.7.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
